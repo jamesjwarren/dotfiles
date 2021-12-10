90a5f146dfe1fcace9c7e515608fd80001cfc0b4
James Warren <james@thisissoon.com>
2021-12-10 19:16:06 +0000

    fix(zsh): Bootstrap zsh-autosuggestions plugin

--- bootstrap.sh
+++ bootstrap.sh
@@ -23,6 +23,7 @@ then
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 	rm ~/.zshrc
 	# Custom plugins
+	git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 fi
 
